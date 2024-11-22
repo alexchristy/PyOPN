9b9f4a06188b088c686503b74757a5fd3caf0750
Alex Christy <a.christy@ufl.edu>
2024-11-22 12:36:06 -0500

    Add kea dhcpv4 and ctrl_agent controllers

---
+++ pyopnsense/core/kea_namespace.py
@@ -0,0 +1,43 @@
+from typing import Optional
+from pyopnsense.base_namespace import BaseNamespace
+
+# Import the client class
+from pyopnsense.core.kea import (
+    CtrlAgentClient,
+    Dhcpv4Client,
+    Leases4Client,
+    ServiceClient
+)
+
+class KeaNamespace(BaseNamespace):
+    """Namespace for Kea related API clients."""
+
+    # Internal attribute for lazy initialization
+    _ctrl_agent: Optional[CtrlAgentClient] = None
+    _dhcpv4: Optional[Dhcpv4Client] = None
+    _leases4: Optional[Leases4Client] = None
+    _service: Optional[ServiceClient] = None
+
+    @property
+    def ctrl_agent(self) -> CtrlAgentClient:
+        if not self._ctrl_agent:
+            self._ctrl_agent = self._initialize_client("ctrl_agent", CtrlAgentClient)
+        return self._ctrl_agent
+    
+    @property
+    def dhcpv4(self) -> Dhcpv4Client:
+        if not self._dhcpv4:
+            self._dhcpv4 = self._initialize_client("dhcpv4", Dhcpv4Client)
+        return self._dhcpv4
+    
+    @property
+    def leases4(self) -> Leases4Client:
+        if not self._leases4:
+            self._leases4 = self._initialize_client("leases4", Leases4Client)
+        return self._leases4
+
+    @property
+    def service(self) -> ServiceClient:
+        if not self._service:
+            self._service = self._initialize_client("service", ServiceClient)
+        return self._service
